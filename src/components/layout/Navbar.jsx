9a87df4e454cadd5c42966e788a8728730edb330
Patrick Marin <patrickmarin@gmail.com>
2022-01-15 16:19:52 -0500

    Install Tailwind and Daisy UI

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,12 @@
+import {FaGithub} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
