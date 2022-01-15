34d13ad79a0d60b13880dee993a6665dbfda64a7
Patrick Marin <patrickmarin@gmail.com>
2022-01-15 16:40:23 -0500

    Footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
-
+import Footer from './components/layout/Footer';
 function App() {
   return (
     
     <Router>
-      <div className = '.flex.flex-col.justify-between.h-screen'>
+      <div className = 'flex flex-col justify-between h-screen'>
         <Navbar />
-
-        <main>
-
+        <main className = 'container mx-auto px-3 pb-12'>
+          Content
         </main>
+        <Footer />
       </div>
+      
     </Router>
   );
 }
