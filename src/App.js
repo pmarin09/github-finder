badf86f0f582c00b6583a1679f1fefc0f92f4c27
Patrick Marin <patrickmarin@gmail.com>
2022-01-15 16:53:06 -0500

    Not Found

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
     
@@ -9,7 +13,12 @@ function App() {
       <div className = 'flex flex-col justify-between h-screen'>
         <Navbar />
         <main className = 'container mx-auto px-3 pb-12'>
-          Content
+          <Routes>
+            <Route path ='/' element={<Home />} />
+            <Route path ='/about' element={<About />} />
+            <Route path ='/notfound' element={<NotFound />} />
+            <Route path ='/*' element={<NotFound />} />
+          </Routes>
         </main>
         <Footer />
       </div>
