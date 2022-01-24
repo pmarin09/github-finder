cb972265559ca3786d902e8750886d77ca52b2b1
Patrick Marin <patrickmarin@gmail.com>
2022-01-24 07:28:17 -0500

    Github Profile DataFrontEnd Tailwind Daisy UI

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -23,7 +23,7 @@ console.log(users)
 
   
     return (
-        <div className = 'grid grid-cols1 xl:grid-cols2 lg:grid-cols2 md:grid-cols-2 mb-8 gap-8'>
+        <div className = 'grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
             <div>
                 <form onSubmit={handleSubmit}>
                     <div classname= 'form-control'>
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
+        user: {},
         loading: false,
     }
 
@@ -38,6 +39,32 @@ export const GithubProvider = ({children}) => {
             })
       }
 
+
+      //Get single user
+    const getUser = async (login) => {
+        setLoading()
+   
+    const response = await fetch(
+        `${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `token${GITHUB_TOKEN}`
+            },
+        })
+
+        if(response.status === 404) {
+            window.location = './notfound'
+        }else{
+            const data = await response.json()
+        
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            })
+        }
+
+        
+  }
+
       //Clear User Results
       const clearUsers = () => {
         dispatch({
@@ -50,8 +77,10 @@ export const GithubProvider = ({children}) => {
       return <GithubContext.Provider value = {{
           users: state.users,
           loading: state.loading,
+          user: state.user,
           searchUsers,
-          clearUsers
+          clearUsers,
+          getUser
       }}>
           {children}
       </GithubContext.Provider>
