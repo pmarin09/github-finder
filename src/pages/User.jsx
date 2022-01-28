b686308b45833b3e9a0b4accf8b0d5c304e2b188
Patrick Marin <patrickmarin@gmail.com>
2022-01-28 13:57:37 -0500

    AXIOS Install and Refactoring Github Actions

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,7 +4,7 @@ import GithubContext from '../context/github/GithubContext'
 import {useParams, Link} from 'react-router-dom'
 import Spinner from "../components/layout/Spinner"
 import RepoList from '../components/repos/RepoList'
-import {getUser, getUserRepos} from '../context/github/GithubActions'
+import {getUserAndRepos} from '../context/github/GithubActions'
 function User() {
     const {user, loading, repos, dispatch} = useContext(GithubContext)
 
@@ -13,15 +13,9 @@ function User() {
     useEffect(()=>{
         dispatch({type: 'SET_LOADING'})
       const getUserData = async() => {
-        const userData = await getUser(params.login)
-        dispatch({type: 'GET_USER', payload: userData})
+        const userData = await getUserAndRepos(params.login)
+        dispatch({type: 'GET_USER_AND_REPOS', payload: userData})
       }  
-
-      const getRepoData = async() => {
-        const userRepoData = await getUserRepos(params.login)
-        dispatch({type: 'GET_REPOS', payload: userRepoData})
-      }  
-
       getUserData()
     },[dispatch, params.login])
     const {
